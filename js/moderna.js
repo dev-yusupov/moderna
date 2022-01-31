9f025b92190c4232f096c6a29c0dff49b56a0d78
Bobur Yusupov <boburinfo2006@gmail.com>
2022-01-31 21:26:39 +0500

    Added justify-cntent to project

--- js/moderna.js
+++ js/moderna.js
@@ -1,11 +1,22 @@
 const hamburger = document.querySelector('.hamburger');
-const navItems = document.querySelector('.nav-items')
+const navItems = document.querySelector('.nav-items');
+const nav = document.querySelector('#nav');
+
+//Function for Toggling Classes in this project
+function toggleClass(element, className, existingClass) {
+	if(element.classList.contains(className)) {
+		element.classList.remove(className);
+		element.classList.add(existingClass);
+	}else{
+		element.classList.add(className);
+		element.classList.remove(existingClass);
+	}
+}
 
 hamburger.onclick = function () {
-	if (navItems.style.display != "initial") {
-		navItems.style.display = "initial";
-	}
-	else {
-		navItems.style.display = "none";
-	}
+	toggleClass(navItems, 'nav-items-active', 'nav-items');
+}
+
+nav.onscroll = function() {
+	nav.classList.add('nav-active');
 }
